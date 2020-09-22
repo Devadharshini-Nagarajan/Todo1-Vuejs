2326d66a23bd020888f0b23617453fefd8a1c355
Devadharshini N <devadharshini.n@dcube.infra>
2020-09-22 18:06:50 +0530

    initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+    <input type="checkbox" v-on:change="changeComplete" />
+    <p>
+      {{todo.title}}
+      <button v-on:click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    changeComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: lightgray;
+  border-bottom: 1px solid black;
+  padding: 10px;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: coral;
+  color: white;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>First app created with Vuejs</h1>
+  </div>
+</template>
